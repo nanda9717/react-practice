caf7e6ce60a0ee3dc8bd055088f5b619e2d43a47
Narendra Kumar <narendrasingh909@gmail.com>
2024-05-04 10:41:41 +0530

    Responsive Header Changes

--- src/pages/Layout/Header.js
+++ src/pages/Layout/Header.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-          </ul>
-        </nav>
+    <div className='header-section'>
+        <header>
+            <div className='logo-section'>
+                <p className='logo'>LOGO</p>
+                <div>
+                    <label htmlFor="check" id="check-label"><i className='fa fa-bars'></i></label>
+                </div>
+            </div>
+            <nav className='menu'>
+                <input type="checkbox" id="check" />
+                <ul id='menu-list'>
+                    <li><NavLink exact activeClassName="active" to="/">Home</NavLink></li>
+                    <li><NavLink activeClassName="active" to="/about">About</NavLink></li>
+                    <li><NavLink activeClassName="active" to="/contact">Contact</NavLink></li>
+                </ul>
+            </nav>
+            <div className='login-btns'>
+                <button className='login-btn'>Login</button>
+                <button className='signup-btn'>Sign Up</button>
+            </div>
+        </header>
+    </div>
   )
 }
